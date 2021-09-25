771165a750906159467e8567fcb31d63792c1fd5
Andriy <greenke74@gmail.com>
2021-09-25 12:37:30 +0300

    Added exercise functions

--- IntelliJ projects/src/teacher.java
+++ IntelliJ projects/src/teacher.java
@@ -6,20 +6,20 @@ public class teacher { // викладач
     private int _age; // вік
     private int _experience; // стаж роботи
 
-    public void setId(int id) { // встановлення id
+        //setters
+    public void setId(int id) {
         _id = id;
     }
-
-    public void setSurname(String surname) { //встановлення прізвища
+    public void setSurname(String surname) {
         _surname=surname;
     }
-    public void setName(String name) { //встановлення ім'я
+    public void setName(String name) {
         _name=name;
     }
-    public void setFathersName(String fathersName) { //встановлення ім'я по-батькові
+    public void setFathersName(String fathersName) {
         _fathersName=fathersName;
     }
-    public void setAge(int age) { //встановлення віку
+    public void setAge(int age) {
         if(age>21){
             _age=age;
         }
@@ -27,7 +27,7 @@ public class teacher { // викладач
             System.out.println(" Incorrect age for teacher! ");
         }
     }
-    public void setExperience(int experience) { //встановлення стажу
+    public void setExperience(int experience) {
         if(experience>0) {
             if(experience<_age) {
                 _experience=experience;
@@ -38,10 +38,10 @@ public class teacher { // викладач
         }
     }
 
+        //getters
     public int getId() {
         return _id;
     }
-
     public String getSurname() {
         return _surname;
     }
@@ -57,7 +57,8 @@ public class teacher { // викладач
     public int getExperience() {
         return _experience;
     }
-    teacher() {}; // порожній конструктор
+        //constructors
+    teacher() {} // порожній конструктор
     teacher(int id, String surname, String name, String fathersName, int age, int experience){ // конструктор з параметрами
         _id=id;
         _surname=surname;
@@ -66,10 +67,11 @@ public class teacher { // викладач
         _age=age;
         _experience=experience;
     }
-    public void print() { // вивід інформації в нижньому реєстрі
+        //information printers
+    public void print() {
         System.out.println("teacher: " +  _surname + " " + _name + " " + _fathersName + " age: " + _age + " experience: " + _experience);
     }
-    public void upperPrint() { // вивід інформації в верхньому реєстрі
+    public void upperPrint() {
         System.out.println("TEACHER: " +  _surname.toUpperCase() + " " + _name.toUpperCase() + " " + _fathersName.toUpperCase() + " AGE: " + _age + " EXPERIENCE: " + _experience);
     }
 }
